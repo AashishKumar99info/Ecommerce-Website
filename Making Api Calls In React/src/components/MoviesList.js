d2683df1f06ef0a2602017472dd9817543adef25
aashishkumar99info <aashishkumar99info@gmail.com>
2023-04-14 15:52:58 +0530

    Task-11 Storing and deleting the new movies into the Databases

--- Making Api Calls In React/src/components/MoviesList.js
+++ Making Api Calls In React/src/components/MoviesList.js
@@ -4,14 +4,17 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
+
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
+          id={movie.id}  // added
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onDeleteMovie={props.onDeleteMovie} //added
         />
       ))}
     </ul>
