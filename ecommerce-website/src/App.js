7888213bcaa579e31574fed091bf5be4e20f3d2a
aashishkumar99info <aashishkumar99info@gmail.com>
2023-04-19 20:22:28 +0530

    Task-23 product details page is proteted Or Protecting the product page

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -57,7 +57,7 @@ function App() {
           <Route path='/about' element={<AboutPage />} />
           <Route path='/contact' element={<ContactUs />} />
           <Route path='user/:userID' element={<ProfilePage />} />
-          <Route path='/products/:productID' element={<ProductDetails />} />
+          <Route path='/products/:productID/:idToken' element={<ProductDetails />} />
         </Routes>
         {cartOpen && <CartContainer></CartContainer>}
       </CartContext.Provider>
